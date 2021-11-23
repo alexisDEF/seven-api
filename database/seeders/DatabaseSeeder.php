e1311466737cb65f7f7400ac29fecc21be7fdef1
Cyprien <rimbau15cy@lycee-ndduroc.com>
2021-11-23 10:36:29 +0100

    login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'email'=>'01@sevenapi.fr',
+            'password'=>Hash::make('12345678'),
+            'name' => '01',
+        ]);
+         \App\Models\User::factory(10)->create();
+
+         $saler = Role::create(['name'=>'saler']);
+         $purchaser = Role::create(['name'=>'purchaser']);
+         $stockEmployee = Role::create(['name'=>'stockEmployee']);
+         $stockManager = Role::create(['name'=>'stockManager']);
+
+         $addStockWithdrawal = Permission::create(['name'=>'addStockWithdrawal']);
+         $addStockEntry = Permission::create(['name'=>'addStockEntry']);
+         $addSupplierOrder = Permission::create(['name'=>'addSupplierOrder']);
+         $addDelivery = Permission::create(['name'=>'addDelivery']);
+
+         $saler->givePermissionTo('addStockWithdrawal');
+         $purchaser->givePermissionTo('addSupplierOrder');
+         $stockEmployee->givePermissionTo('addStockWithdrawal','addStockEntry');
+         $stockManager->givePermissionTo('addStockWithdrawal','addStockEntry','addDelivery');
+
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('login', [\App\Http\Controllers\API\UserController::class,'login']);
+Route::post('register', [\App\Http\Controllers\API\UserController::class,'register']);
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('getRegisteredUser', 'API\UserController@getRegisteredUser');
 });
+
+Route::get('user',[\App\Http\Controllers\API\UserController::class,'index']);
